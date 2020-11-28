2df76ba13d61c1d342013b6bd6a819eacd6f9a29
rfaelfp <54748929+rfaelfp@users.noreply.github.com>
2020-11-28 18:16:46 -0300

    Adicionando header no acompanhamento

--- projeto_anima/lib/screens/body/acompanhamento.dart
+++ projeto_anima/lib/screens/body/acompanhamento.dart
@@ -29,49 +29,69 @@ class _AcompanhamentoState extends State<Acompanhamento> {
               );
             } else if (snapshot.data['acompanhamento'] == false) {
               return ListView(
-                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
                 children: [
+                  Container(
+                    child: Center(
+                      child: Text(
+                        'Acompanhamento',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            shadows: [
+                              Shadow(blurRadius: 4.0, color: Colors.white)
+                            ]),
+                      ),
+                    ),
+                    color: const Color(0xFF363a7b),
+                    height: 50.0,
+                  ),
                   Form(
                       key: _formKey,
-                      child: Column(
-                        children: [
-                          Text(
-                            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?',
-                          ),
-                          SizedBox(
-                            height: 20.0,
-                          ),
-                          TextFormField(
-                            focusNode: myFocusNode,
-                            cursorColor: Colors.red[400],
-                            decoration: InputDecoration(
-                                labelText: "Resposta",
-                                labelStyle: TextStyle(
-                                    color: myFocusNode.hasFocus
-                                        ? Colors.black
-                                        : Colors.red[400]),
-                                fillColor: Colors.white,
-                                focusedBorder: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(20.0),
-                                    borderSide:
-                                        BorderSide(color: Colors.red[400])),
-                                border: OutlineInputBorder(
-                                    borderSide:
-                                        BorderSide(color: Colors.red[400]),
-                                    borderRadius: BorderRadius.circular(20.0))),
-                            keyboardType: TextInputType.multiline,
-                            maxLines: 10,
-                            validator: (val) =>
-                                val.isEmpty ? "Preencher a resposta" : null,
-                            onChanged: (val) {
-                              setState(() => resposta = val);
-                            },
-                          )
-                        ],
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 15.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua?',
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            TextFormField(
+                              focusNode: myFocusNode,
+                              cursorColor: const Color(0xFF0098fc),
+                              decoration: InputDecoration(
+                                  labelText: "Resposta",
+                                  labelStyle: TextStyle(
+                                      color: myFocusNode.hasFocus
+                                          ? Colors.black
+                                          : const Color(0xFF0098fc)),
+                                  fillColor: Colors.white,
+                                  focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(20.0),
+                                      borderSide: BorderSide(
+                                          color: const Color(0xFF0098fc))),
+                                  border: OutlineInputBorder(
+                                      borderSide: BorderSide(
+                                          color: const Color(0xFF0098fc)),
+                                      borderRadius:
+                                          BorderRadius.circular(20.0))),
+                              keyboardType: TextInputType.multiline,
+                              maxLines: 10,
+                              validator: (val) =>
+                                  val.isEmpty ? "Preencher a resposta" : null,
+                              onChanged: (val) {
+                                setState(() => resposta = val);
+                              },
+                            )
+                          ],
+                        ),
                       )),
                   Center(
                     child: RaisedButton(
-                        color: Colors.red[400],
+                        color: const Color(0xFF0098fc),
                         child: Text(
                           'Enviar',
                           style: TextStyle(color: Colors.white),
